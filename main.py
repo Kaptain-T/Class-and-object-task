16aee8e08dad16c4176d26724bb641fd5ccdbc06
Kaptain-T <104356510+Kaptain-T@users.noreply.github.com>
2022-05-31 19:59:45 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age, track, score):
+    #constructor
+    def __init__(self, name, age, tracks, score):
         self.name = name
         self.age = age
-        self.track = track
+        self.tracks = tracks
         self.score = score
 
-    def change_name(self):
-        pass
+#method for changing object name
+    def change_name(self, new_name):
+        self.name = new_name
+        return
 
-    def change_age(self):
-        pass
+#method for changing object age as integer
+    def change_age(self, new_age):
+        new_age= int(new_age)
+        self.age = new_age
+        return
 
-    
+#method for adding new track to object
+    def add_track (self, new_track):
+        self.tracks = self.tracks.append(new_track)
+
+#method for changing object score
+    def change_score(self, new_score):
+        self.score = new_score
+        return
+
+#method for getting object score
+    def get_score (self):
+        print (self.score)
+
+#Creating an object "Anthony" with the class "Student"
+Anthony = Student(name="Anthony", age=22, tracks=["Fullstack","Blockchain"], score=29.5)
 
 
+Anthony.change_name("Kwesi")
+print(Anthony.name)
 
-    
+
+Anthony.change_age(32.9)
+print (Anthony.age)
 
 
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+Anthony.change_score(45)
+Anthony.get_score()
 
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+
+Anthony.add_track("Devops")
+
+
